ffc7af4e2ee18e08574e6d0e4edb3ae1c9913952
Rachid <Rachid.Bouadi@adesso.de>
2024-02-02 18:05:34 +0100

    working on landmark controller tests

--- landmarks-api/src/landmarks/landmarks.controller.spec.ts
+++ landmarks-api/src/landmarks/landmarks.controller.spec.ts
@@ -4,6 +4,7 @@ import { LandmarksService } from './landmarks.service';
 
 describe('LandmarksController', () => {
   let controller: LandmarksController;
+  let service: LandmarksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,149 @@ describe('LandmarksController', () => {
     }).compile();
 
     controller = module.get<LandmarksController>(LandmarksController);
+    service = module.get<LandmarksService>(LandmarksService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of landmarks', () => {
+      const landmarkList = [
+        {
+          id: 1,
+          name: 'Kasbah of Algiers',
+          country: 'Algeria',
+          location: 'Algiers',
+          historical_context: null,
+          year_built: null,
+          architect: null,
+          image_url: null,
+          created_at: new Date('2024-01-31'),
+          updated_at: new Date('2024-01-31'),
+        },
+        {
+          id: 2,
+          name: 'Timgad',
+          country: 'Algeria',
+          location: 'Batna Province',
+          historical_context: null,
+          year_built: null,
+          architect: null,
+          image_url: null,
+          created_at: new Date('2024-01-31'),
+          updated_at: new Date('2024-01-31'),
+        },
+      ];
+
+      jest
+        .spyOn(service, 'findAll')
+        .mockImplementation(() => Promise.resolve(landmarkList));
+
+      expect(controller).toBeDefined();
+      expect(controller.findAll()).toBe(landmarkList);
+    });
+  });
+
+  describe('createLandmark', () => {
+    it('should create a new landmark', async () => {
+      const createdLandmark = {
+        id: 1,
+        name: 'Kasbah of Algiers',
+        country: 'Algeria',
+        location: 'Algiers',
+        historical_context: null,
+        year_built: null,
+        architect: null,
+        image_url: null,
+        created_at: new Date('2024-01-31'),
+        updated_at: new Date('2024-01-31'),
+      };
+
+      jest
+        .spyOn(service, 'create')
+        .mockImplementation(() => Promise.resolve(createdLandmark));
+
+      expect(await controller.createLandmark(createdLandmark)).toBe(
+        createdLandmark,
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a specific landmark', async () => {
+      const landmarkId = '1';
+
+      const foundLandmark = {
+        id: 1,
+        name: 'Kasbah of Algiers',
+        country: 'Algeria',
+        location: 'Algiers',
+        historical_context: null,
+        year_built: null,
+        architect: null,
+        image_url: null,
+        created_at: new Date('2024-01-31'),
+        updated_at: new Date('2024-01-31'),
+      };
+
+      jest
+        .spyOn(service, 'findOne')
+        .mockImplementation(() => Promise.resolve(foundLandmark));
+
+      expect(await controller.findOne(landmarkId)).toBe(foundLandmark);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a landmark', async () => {
+      const landmarkId = '1';
+      const updateLandmarkDto = {
+        // ... (provide necessary data for the update)
+      };
+
+      const updatedLandmark = {
+        id: 1,
+        name: 'Kasbah of Algiers',
+        country: 'Algeria',
+        location: 'Algiers',
+        historical_context: null,
+        year_built: null,
+        architect: null,
+        image_url: null,
+        created_at: new Date('2024-01-31'),
+        updated_at: new Date('2024-01-31'),
+      };
+
+      jest
+        .spyOn(service, 'update')
+        .mockImplementation(() => Promise.resolve(updatedLandmark));
+
+      expect(await controller.update(landmarkId, updateLandmarkDto)).toBe(
+        updatedLandmark,
+      );
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a landmark', async () => {
+      const landmarkId = '1';
+
+      const removedLandmark = {
+        id: 1,
+        name: 'Kasbah of Algiers',
+        country: 'Algeria',
+        location: 'Algiers',
+        historical_context: null,
+        year_built: null,
+        architect: null,
+        image_url: null,
+        created_at: new Date('2024-01-31'),
+        updated_at: new Date('2024-01-31'),
+      };
+
+      jest
+        .spyOn(service, 'remove')
+        .mockImplementation(() => Promise.resolve(removedLandmark));
+
+      expect(await controller.remove(landmarkId)).toBe(removedLandmark);
+    });
   });
 });
